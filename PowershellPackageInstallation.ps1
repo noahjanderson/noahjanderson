35dad30cc8a2dab1cb52ba3595d1d9b0af61a9ad
Noah.Anderson <Noah.Anderson@jdpa.com>
2019-12-11 14:19:11 -0800

    updated site name author and created buildspec.yml

---
+++ PowershellPackageInstallation.ps1
@@ -0,0 +1,10 @@
+﻿#install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#install git
+choco install git -y
+#install openssh
+choco install openssh -y -params '/SSHAgentFeature'
+# nodejs 
+choco install nodejs npm -y
+#install atom
+choco install atom -y
